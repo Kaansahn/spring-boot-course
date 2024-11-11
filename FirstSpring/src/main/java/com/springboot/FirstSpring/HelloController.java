3c541d9d864855a442f5b2e0745a66adbb9b90df
Kaan <kaan.sahin2@hotmail.com>
2024-11-11 23:07:01 +0300

    creating a POST request

--- FirstSpring/src/main/java/com/springboot/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/springboot/FirstSpring/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,12 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello" + name + "!";
     }
 
 }
