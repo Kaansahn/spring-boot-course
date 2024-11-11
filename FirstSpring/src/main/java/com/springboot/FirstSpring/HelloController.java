48b4e82f30182c6789ad287e846d192e8e73567a
Kaan <kaan.sahin2@hotmail.com>
2024-11-11 23:23:59 +0300

    HelloResponse

--- FirstSpring/src/main/java/com/springboot/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/springboot/FirstSpring/HelloController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public HelloResponse hello(){
+        return new HelloResponse("Hello World!");
     }
 
     @PostMapping("/hello")
-    public String helloPost(@RequestBody String name){
-        return "Hello" + name + "!";
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello " + name + "!");
     }
 
 }
