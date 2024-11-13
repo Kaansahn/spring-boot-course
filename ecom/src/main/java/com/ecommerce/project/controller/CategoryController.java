0d0679d215ad4a0d56f2759f18e25d334cb80bcb
Kaan <kaan.sahin2@hotmail.com>
2024-11-14 01:30:47 +0300

    valid annotation

--- ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,13 +2,13 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,7 +28,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully!", HttpStatus.CREATED);
     }
@@ -48,7 +48,7 @@ public class CategoryController {
     @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
         try{
-            Category savedCategory = categoryService.updateCategory(category, categoryId);
+            categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
         }catch (ResponseStatusException e){
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
